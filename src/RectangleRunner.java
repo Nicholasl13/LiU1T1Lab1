7138b7a11aa35a34dfe870dfed62fe79d9a86899
1E10 Laptop <sample@bths.edu>
2022-09-23 10:38:09 -0400

    Commit

---
+++ src/RectangleRunner.java
@@ -0,0 +1,19 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+        Rectangle rect3 = new Rectangle(3,4);
+        rect3.printArea();
+
+        Rectangle rect2 = new Rectangle(7,8);
+        rect2.printArea();
+
+        Rectangle rect1 = new Rectangle(5,6);
+        rect1.printArea();
+
+        rect3.printPerimeter();
+        rect2.printPerimeter();
+        rect1.printPerimeter();
+
+
+
+    }
+}
